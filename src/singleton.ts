e16913f60f89532e52cdf76cc9dd33557e3124c2
Jeremy Lanssiers <jlanssie@gmail.com>
2025-08-16 23:04:35 +0200

    Adapted Singleton Class

--- src/singleton.ts
+++ src/singleton.ts
@@ -1,14 +1,24 @@
+/**
+ * Class representing a singleton pattern.
+ * Inspired by [Tomasz Buszewski's article on JavaScript Singletons](https://dev.to/tomekbuszewski/singleton-in-javascript-1d5i).
+ */
 export class Singleton {
   private static instance: any;
+  private readonly immutableData: any;
   private data: any;
 
-  constructor() {
+  constructor(immutableData: any = null) {
     if (Singleton.instance) {
       return Singleton.instance;
     }
 
     Singleton.instance = this;
+    this.immutableData = immutableData;
     return this;
+  }
+
+  getImmutableData(): any {
+    return this.immutableData;
   }
 
   getData(): any {
--- test/singleton.test.ts
+++ test/singleton.test.ts
@@ -1,11 +1,33 @@
 import { Singleton } from '../src/singleton';
 
 describe('Singleton', () => {
-  it('should allow only one instance of a class', () => {
+  test('A Singleton Class should create only one instance.', () => {
+    const instanceOne = new Singleton('Hello, World!');
+    const instanceTwo = new Singleton('Hello, World!!');
+    const instanceThree = new Singleton('Hello, World!!!');
+
+    expect(instanceOne).toBe(instanceTwo);
+    expect(instanceTwo).toBe(instanceThree);
+    expect(instanceThree).toBe(instanceOne);
+  });
+
+  test('A Singleton Class constructor should only be called once.', () => {
+    const instanceOne = new Singleton('Hello, World!');
+    const instanceTwo = new Singleton('Hello, World!!');
+    const instanceThree = new Singleton('Hello, World!!!');
+
+    expect(instanceOne.getImmutableData()).toBe('Hello, World!');
+    expect(instanceTwo.getImmutableData()).toBe('Hello, World!');
+    expect(instanceThree.getImmutableData()).toBe('Hello, World!');
+  });
+
+  test('All Singleton Class instance references should return the same data.', () => {
     const instanceOne = new Singleton();
     instanceOne.setData('Hello, World!');
+
     const instanceTwo = new Singleton();
     instanceTwo.setData('Hello, World!!');
+
     const instanceThree = new Singleton();
     instanceThree.setData('Hello, World!!!');
 
