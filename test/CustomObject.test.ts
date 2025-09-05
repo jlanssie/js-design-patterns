b7371626450640038363dad86d5c2c1859404a2d
Jeremy Lanssiers <jlanssie@gmail.com>
2025-09-05 15:56:00 +0200

    Refactored Product, Factory and Abastract Factory

--- test/CustomObject.test.ts
+++ test/CustomObject.test.ts
@@ -1,13 +1,13 @@
 import { CustomObject } from '../src/CustomObject.class';
 
 describe('Custom Object', () => {
-  test('A product can be created with empty data.', () => {
+  test('A custom object can be created with empty data.', () => {
     const product = new CustomObject();
 
     expect(product.data).toBeNull();
   });
 
-  test('A product can be created with data.', () => {
+  test('A custom object can be created with data.', () => {
     const product = new CustomObject('Data');
 
     expect(product.data).toBe('Data');
