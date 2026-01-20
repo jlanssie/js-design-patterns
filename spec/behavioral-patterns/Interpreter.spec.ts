43f7312b0c1b95db7b8fe9cdf1ae58309df89aa2
Jeremy Lanssiers <jlanssie@gmail.com>
2026-01-20 19:25:50 +0100

    update

---
+++ spec/behavioral-patterns/Interpreter.spec.ts
@@ -0,0 +1,7 @@
+describe('Interpreter', () => {
+  test('An Interpreter accepts a context. It forwards the context to a terminal expression or a non-terminal expression, which in turn functions as an interpreter that either forwards the context further to a terminal expression or another non-terminal expression.', () => {
+    // const context
+    // const interpreter
+    // interpreter.interpret(context);
+  });
+});
